b9ff84bb473fdd9d399a38c4321b8f54074d8660
erikvw <ew2789@gmail.com>
2023-05-24 17:33:59 -0500

    model admin mixins

--- edc_qol/admin/__init__.py
+++ edc_qol/admin/__init__.py
@@ -1,3 +1,3 @@
-from .eq5d3l_admin import Eq5d3lAdmin, eq5d3l_fieldsets, eq5d3l_radio_fields
-from .icecapa_admin import IcecapaAdmin, icecapa_fieldsets, icecapa_radio_fields
+from .eq5d3l_admin import Eq5d3lAdmin
+from .icecapa_admin import IcecapaAdmin
 from .sf12_admin import Sf12Admin, sf12_fieldsets, sf12_radio_fields
