349bd6d8a3dca225e730b48bc9f64cfc9d3b6b0f
erikvw <ew2789@gmail.com>
2024-11-04 05:59:28 -0600

    do not include on administration page

--- edc_qol/apps.py
+++ edc_qol/apps.py
@@ -5,4 +5,4 @@ class AppConfig(DjangoAppConfig):
     name = "edc_qol"
     verbose_name = "Edc Quality of Life (QoL)"
     has_exportable_data = True
-    include_in_administration_section = True
+    include_in_administration_section = False
