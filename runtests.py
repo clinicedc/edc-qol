45e1a6450ce7bfd608924aed3b4b9f4cdafb2277
erikvw <ew2789@gmail.com>
2025-10-03 17:38:30 -0500

    fix regression when conparing visual slider with confirmed value

--- runtests.py
+++ runtests.py
@@ -2,4 +2,4 @@
 from edc_test_settings.func_main import func_main2
 
 if __name__ == "__main__":
-    func_main2("tests.test_settings", "tests")
+    func_main2("tests.test_settings", "edc_qol.tests")
