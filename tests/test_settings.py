0aacc1742ab97d1e75d496ff70e4e19f8530a751
erikvw <ew2789@gmail.com>
2025-10-28 22:52:34 -0500

    fix tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -15,6 +15,7 @@ project_settings = DefaultTestSettings(
         "edc_sites.E001",
         "edc_sites.E002",
         "edc_navbar.E003",
+        "edc_navbar.E004",
         "edc_consent.E001",
     ],  # The SITE_ID setting must be an integer
     INSTALLED_APPS=[
@@ -22,6 +23,7 @@ project_settings = DefaultTestSettings(
     ],
     add_dashboard_middleware=True,
     add_lab_dashboard_middleware=True,
+    DJANGO_REVISION_IGNORE_WORKING_DIR=True,
 ).settings
 
 for k, v in project_settings.items():
